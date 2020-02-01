f4d0e159828d9ef7c75d4de16309b091e8e37754
classy <classy@cs.ubc.ca>
2020-02-01 23:19:38 +0000

    Starter files

---
+++ src/main/ca/ubc/cpsc210/helpdesk/model/IncidentQueue.java
@@ -0,0 +1,8 @@
+package ca.ubc.cpsc210.helpdesk.model;
+
+// Represents a queue of incidents to be handled by helpdesk
+// with maximum size MAX_SIZE
+public class IncidentQueue {
+    public static final int MAX_SIZE = 10;
+    // TODO: complete the design of the IncidentQueue class
+}
---
+++ src/test/ca/ubc/cpsc210/helpdesk/test/IncidentQueueTest.java
@@ -0,0 +1,7 @@
+package ca.ubc.cpsc210.helpdesk.test;
+
+
+class IncidentQueueTest {
+    // Design your unit tests for the IncidentQueue class here
+
+}
