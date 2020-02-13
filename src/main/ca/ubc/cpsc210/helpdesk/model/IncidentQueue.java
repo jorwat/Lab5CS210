3d9d0dddc92c35aaafd8e08281fab082543620c3
Jordan <v3y3b@ugrad.cs.ubc.ca>
2020-02-12 22:49:50 -0800

    Implemented Incident Queue, testing now

--- src/main/ca/ubc/cpsc210/helpdesk/model/IncidentQueue.java
+++ src/main/ca/ubc/cpsc210/helpdesk/model/IncidentQueue.java
@@ -1,8 +1,79 @@
 package ca.ubc.cpsc210.helpdesk.model;
 
+import java.util.LinkedList;
+
 // Represents a queue of incidents to be handled by helpdesk
 // with maximum size MAX_SIZE
 public class IncidentQueue {
     public static final int MAX_SIZE = 10;
-    // TODO: complete the design of the IncidentQueue class
+
+    private LinkedList<Incident> queue;
+
+    // EFFECTS: creates an empty incident queue
+    public IncidentQueue() {
+        queue = new LinkedList<>();
+    }
+
+    // REQUIRES: queue cannot be full
+    // MODIFIES: this
+    // EFFECTS: if the queue is not full, adds an incident to the end of the queue and returns true.
+    //          if queue is full, return false
+    public boolean addIncident(Incident incident) {
+        boolean state;
+        if (isFull()) {
+            state = false;
+        } else {
+            queue.addLast(incident);
+            state = true;
+        }
+        return state;
+    }
+
+    // REQUIRES: queue cannot be empty
+    // MODIFIES: this
+    // EFFECTS: removes the incident from the front of the queue and returns it
+    public Incident getNextIncident() {
+        Incident i;
+        i = queue.getFirst();
+        queue.removeFirst();
+        return i;
+    }
+
+    // EFFECTS: takes a case number and returns the position in the queue of the incident with that number
+    //          if there is no incident with that case number, the method returns -1
+    public int getPositionInQueueOfCaseNumber(int num) {
+        int pos = 0;
+
+        for (Incident i : queue) {
+            if (num == i.getCaseNum()) {
+                pos = queue.indexOf(i) + 1;
+            } else if (pos == 0) {
+                pos = -1;
+            }
+        }
+        return pos;
+    }
+
+    // EFFECTS: returns length of the queue
+    public int length() {
+        return queue.size();
+    }
+
+    // EFFECTS: returns true is queue is empty, false otherwise
+    public boolean isEmpty() {
+        boolean state = false;
+        if (queue.size() == 0) {
+            state = true;
+        }
+        return state;
+    }
+
+    // EFFECTS: returns true if queue is full, false otherwise
+    public boolean isFull() {
+        boolean state = false;
+        if (queue.size() == MAX_SIZE) {
+            state = true;
+        }
+        return state;
+    }
 }
--- src/test/ca/ubc/cpsc210/helpdesk/test/IncidentQueueTest.java
+++ src/test/ca/ubc/cpsc210/helpdesk/test/IncidentQueueTest.java
@@ -1,7 +1,55 @@
 package ca.ubc.cpsc210.helpdesk.test;
 
 
-class IncidentQueueTest {
-    // Design your unit tests for the IncidentQueue class here
+import ca.ubc.cpsc210.helpdesk.model.Incident;
+import ca.ubc.cpsc210.helpdesk.model.IncidentQueue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class IncidentQueueTest {
+    private IncidentQueue q;
+    private Incident i1;
+    private Incident i2;
+
+    @BeforeEach
+    void runBefore() {
+        q = new IncidentQueue();
+        i1 = new Incident(1, "Broken Bone");
+        i2 = new Incident(5, "Broken Ankle");
+    }
+
+    @Test
+    void testConstructor() {
+        assertFalse(q.isFull());
+        assertTrue(q.isEmpty());
+        assertEquals(0,q.length());
+    }
+
+    @Test
+    void testAddIncident() {
+        q.addIncident(i1);
+        q.addIncident(i2);
+        assertEquals(2,q.length());
+        assertFalse(q.isFull());
+        assertFalse(q.isEmpty());
+    }
+
+    @Test
+    void testGetNextIncident() {
+        q.addIncident(i1);
+        q.addIncident(i2);
+        q.getNextIncident();
+        assertEquals(1,q.length());
+    }
+
+    @Test
+    void testGetPositionOfCase() {
+        q.addIncident(i1);
+        q.addIncident(i2);
+        assertEquals(2,q.getPositionInQueueOfCaseNumber(5));
+        assertEquals(1,q.getPositionInQueueOfCaseNumber(1));
+        assertEquals(-1,q.getPositionInQueueOfCaseNumber(7));
+    }
 }
